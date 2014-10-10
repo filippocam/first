54d6779868b44793bdb356549f6724f4de4831d0
filippocam <campegiani@gmail.com>
2014-10-10 11:57:54 +0200

    test

---
+++ first/first/src/main/java/it/filo/ooad/first/Main.java
@@ -0,0 +1,12 @@
+package it.filo.ooad.first;
+
+/**
+ *
+ * @author filippo.campegiani
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+    
+}
