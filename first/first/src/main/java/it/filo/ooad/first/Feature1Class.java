c7675c862a0113ad865eb9d1f2a246f5ad98e674
filippocam <campegiani@gmail.com>
2014-10-10 12:22:57 +0200

    testtest

--- first/first/src/main/java/it/filo/ooad/first/Feature1Class.java
+++ first/first/src/main/java/it/filo/ooad/first/Feature1Class.java
@@ -1,9 +1,13 @@
 package it.filo.ooad.first;
 
 /**
- *
+ * modificato commento
  * @author filippo.campegiani
  */
 public class Feature1Class {
     
+    // test
+    public static void main(String[] args) {
+        
+    }
 }
